b0da013ff74e075f8ec367f51d270d763340db9a
IAMTANZ~~$$ <namankhurana9083@gmail.com>
2023-06-18 00:27:54 +0530

    4th commit

--- src/Pages/HomeComponents/ConfirmationPopUp.jsx
+++ src/Pages/HomeComponents/ConfirmationPopUp.jsx
@@ -7,24 +7,24 @@ import { CovidState } from "../../Config/CovidContext";
 const useStyles = makeStyles((theme) => ({
   modal: {
     display: "flex",
+    flexDirection: "row",
     alignItems: "center",
-    backgroundColor: "rgba(0,0,0,0.5)",
+    backgroundColor: "transparent",
 
     justifyContent: "center",
   },
   field: {
-      display: "flex",
-      justifyContent: "space-between",  
-      alignItems: "center",
-      gap: 10,
-      padding: 10,
-      backgroundColor: "white",
-      color: "black",
-      },
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 10,
+    padding: 10,
+    backgroundColor: "white",
+    color: "black",
+  },
 }));
-const ConfirmationPopUp = ({slot,index}) => {
-  const {confirmOpen, setConfirmOpen} = CovidState();
-  
+const ConfirmationPopUp = ({ slot, index }) => {
+  const { confirmOpen, setConfirmOpen } = CovidState();
 
   const handleClose = () => {
     setConfirmOpen(false);
@@ -32,80 +32,79 @@ const ConfirmationPopUp = ({slot,index}) => {
   const classes = useStyles();
 
   return (
-
-     
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={confirmOpen}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      className={classes.modal}
+      open={confirmOpen}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      {/* Details of user and slot in tabular form*/}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 10,
+          padding: 10,
+          width: "30vw",
+          backgroundColor: "white",
+          color: "black",
+          borderRadius: 10,
         }}
       >
-        {/* Details of user and slot in tabular form*/}
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 10,
-            padding: 10,
-            width: "30vw",
-            backgroundColor: "white",
-            color: "black",
-            borderRadius: 10,
-          }}
-        ><Typography variant="h4" style={{ color: "black" ,textAlign:"center"}}>
-                Slot Booked Successfully
-                </Typography>
-          <div
-             className={classes.field}
-          >
-            <Typography variant="h6" style={{ color: "black" }}>
-              Name:
-            </Typography>
-            <Typography variant="h6" style={{ color: "black" }}>
-              {auth.currentUser.displayName || "Username"}
-            </Typography>
-          </div>
-          <div className={classes.field}>
-            <Typography variant="h6" style={{ color: "black" }}>
-              Email:
-            </Typography>
-            <Typography variant="h6" style={{ color: "black" }}>
-              {auth.currentUser.email}
-            </Typography>
-          </div>
-          <div className={classes.field}>
-            <Typography variant="h6" style={{ color: "black" }}>
-              Slot:
-            </Typography>
-            <Typography variant="h6" style={{ color: "black" }}>
-              {slot.timings[index]}
-            </Typography>
-          </div>
-          <div className={classes.field}>
-            <Typography variant="h6" style={{ color: "black" }}>
-              Date:
-            </Typography>
-            <Typography variant="h6" style={{ color: "black" }}>
-              {new Date().toISOString().slice(0, 10)}
-            </Typography>
-          </div>
-          <div className={classes.field}>
-            <Typography variant="h6" style={{ color: "black" }}>
-              Vaccine
-            </Typography>
-            <Typography variant="h6" style={{ color: "black" }}>
-              {slot.vaccine}
-            </Typography>
-          </div>
+        <Typography
+          variant="h4"
+          style={{ color: "black", textAlign: "center" }}
+        >
+          Slot Booked Successfully
+        </Typography>
+        <div className={classes.field}>
+          <Typography variant="h6" style={{ color: "black" }}>
+            Name:
+          </Typography>
+          <Typography variant="h6" style={{ color: "black" }}>
+            {auth.currentUser.displayName || "Username"}
+          </Typography>
         </div>
-      </Modal>
-  
+        <div className={classes.field}>
+          <Typography variant="h6" style={{ color: "black" }}>
+            Email:
+          </Typography>
+          <Typography variant="h6" style={{ color: "black" }}>
+            {auth.currentUser.email}
+          </Typography>
+        </div>
+        <div className={classes.field}>
+          <Typography variant="h6" style={{ color: "black" }}>
+            Slot:
+          </Typography>
+          <Typography variant="h6" style={{ color: "black" }}>
+            {slot.timings[index]}
+          </Typography>
+        </div>
+        <div className={classes.field}>
+          <Typography variant="h6" style={{ color: "black" }}>
+            Date:
+          </Typography>
+          <Typography variant="h6" style={{ color: "black" }}>
+            {new Date().toISOString().slice(0, 10)}
+          </Typography>
+        </div>
+        <div className={classes.field}>
+          <Typography variant="h6" style={{ color: "black" }}>
+            Vaccine
+          </Typography>
+          <Typography variant="h6" style={{ color: "black" }}>
+            {slot.vaccine}
+          </Typography>
+        </div>
+      </div>
+    </Modal>
   );
 };
 
